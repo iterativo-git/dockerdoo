c786cd87ddc2a51a5c34bb89ff1bb2c4f71a40f5
Gustavo Valverde <g.valverde02@gmail.com>
2019-12-09 23:16:42 -0400

    [IMP] entrypoint: Do not enforce strict scripts and do not send credentials on logs with -x

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -xe
-
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
