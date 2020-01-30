84a3ca49bedced105ab60ea47a2009cb9564afd0
Gustavo Valverde <gustavo@iterativo.do>
2020-01-30 15:22:14 -0400

    [FIX] Do not duplicate the addons_path

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -55,8 +55,7 @@ smtp_user = ${SMTP_USER}
 test_enable = ${TEST_ENABLE}
 unaccent = ${UNACCENT}
 without_demo = ${WITHOUT_DEMO}
-workers = ${WORKERS}
-addons_path = ${ODOO_ADDONS_BASEPATH},${EXTRA_ADDONS_PATHS}" > $ODOO_RC
+workers = ${WORKERS}" > $ODOO_RC
     if [ -z "$EXTRA_ADDONS_PATHS" ]; then
         echo "The variable \$EXTRA_ADDONS_PATHS is empty, using default addons_path"
         echo "addons_path = ${ODOO_ADDONS_BASEPATH}" >> $ODOO_RC
